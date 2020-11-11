fcff29eb8a4a6f931d7fbf966bfcf59a894861ab
benjrozen@gmail.com <b3nr0z3n>
2020-11-11 14:04:17 +0200

    webhookaa1

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ UPLOAD_FOLDER = 'C:/Users/tunke/PycharmProjects/Potlopedia_2.0/static/prod_pics/
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-anni = 7
+anni = 789
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
